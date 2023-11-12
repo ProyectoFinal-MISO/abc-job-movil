854d2621312de719b20764c0d57da0fce706094a
andres.cantor@payu.com <andres.cantor@payu.com>
2023-11-11 23:28:32 -0500

    connect with api but failed in local

--- app/src/main/java/com/example/abc_job_movil/network/NetworkServiceAdapter.kt
+++ app/src/main/java/com/example/abc_job_movil/network/NetworkServiceAdapter.kt
@@ -7,53 +7,28 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import org.json.JSONObject
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class NetworkServiceAdapter constructor(context: Context) {
 
-    companion object{
-        const val BASE_URL = ""
-        var instance: NetworkServiceAdapter? = null
-        fun getInstance(context: Context) =
-            instance ?: synchronized(this) {
-                instance ?: NetworkServiceAdapter(context).also {
-                    instance = it
-                }
-            }
-        fun getRetroInstance(): Retrofit {
-            val logging = HttpLoggingInterceptor()
-            logging.level = HttpLoggingInterceptor.Level.BODY
-            val client = OkHttpClient.Builder()
-            client.addInterceptor(logging)
+    val instance: RequestQueue = Volley.newRequestQueue(context.applicationContext)
 
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(client.build())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+    companion object{
+        //const val BASE_URL = "https://34.117.27.162/"
+        private const val BASE_URL = "http//localhost:5000/"
+        fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
+            return StringRequest(Request.Method.GET, BASE_URL+path, responseListener,errorListener)
+        }
+        fun postRequest(path: String, body: JSONObject, responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ): JsonObjectRequest {
+            return  JsonObjectRequest(Request.Method.POST, BASE_URL+path, body, responseListener, errorListener)
+        }
+        fun deleteRequest(path: String, body: JSONObject, responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ): JsonObjectRequest {
+            return  JsonObjectRequest(Request.Method.DELETE, BASE_URL+path, body, responseListener, errorListener)
+        }
+        fun putRequest(path: String, body: JSONObject, responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
+            return  JsonObjectRequest(Request.Method.PUT, BASE_URL+path, body, responseListener, errorListener)
         }
 
-    }
-
-    private val requestQueue: RequestQueue by lazy {
-        Volley.newRequestQueue(context.applicationContext)
-    }
-
-    private fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
-        return StringRequest(Request.Method.GET, BASE_URL+path, responseListener,errorListener)
-    }
-    private fun postRequest(path: String, body: JSONObject, responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ): JsonObjectRequest {
-        return  JsonObjectRequest(Request.Method.POST, BASE_URL+path, body, responseListener, errorListener)
-    }
-    private fun deleteRequest(path: String, body: JSONObject, responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ): JsonObjectRequest {
-        return  JsonObjectRequest(Request.Method.DELETE, BASE_URL+path, body, responseListener, errorListener)
-    }
-    private fun putRequest(path: String, body: JSONObject, responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
-        return  JsonObjectRequest(Request.Method.PUT, BASE_URL+path, body, responseListener, errorListener)
     }
 
 }
--- app/src/main/java/com/example/abc_job_movil/ui/LoginActivity.kt
+++ app/src/main/java/com/example/abc_job_movil/ui/LoginActivity.kt
@@ -4,15 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import com.android.volley.Response
 import com.example.abc_job_movil.R
+import com.example.abc_job_movil.network.NetworkServiceAdapter
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var networkAdapter: NetworkServiceAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        networkAdapter = NetworkServiceAdapter(this.applicationContext)
 
         val back: ImageView = findViewById(R.id.back)
         back.setOnClickListener{
@@ -20,17 +29,48 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val email: EditText = findViewById(R.id.email)
-        val password: EditText = findViewById(R.id.password)
+        val showLogin: TextView = findViewById(R.id.showLogin)
+        val checkBoxTypeABC: CheckBox = findViewById(R.id.ABCUser)
+        val checkBoxTechnical: CheckBox = findViewById(R.id.technicalResource)
 
-        val login: Button = findViewById(R.id.login)
-        back.setOnClickListener{
-            val intent = Intent(this, HomeTechnicalResourceActivity::class.java)
-            startActivity(intent)
+        checkBoxTypeABC.setOnCheckedChangeListener { _, isChecked ->
+            checkBoxTechnical.isEnabled = !isChecked
         }
 
+        checkBoxTechnical.setOnCheckedChangeListener { _, isChecked ->
+            checkBoxTypeABC.isEnabled = !isChecked
+        }
+
+        val login: Button = findViewById(R.id.login)
+        login.setOnClickListener{
+            val email: EditText = findViewById(R.id.email)
+            val password: EditText = findViewById(R.id.password)
+            var userType: String = "DEFAULT"
+
+            if (checkBoxTypeABC.isChecked) {
+                userType = "EMPLOYEE"
+            } else if (checkBoxTechnical.isChecked) {
+                userType = "PERSON"
+            }
+
+            val postParams = mapOf<String, Any>(
+                "username" to email.text.toString(),
+                "password" to password.text.toString(),
+                "userType" to userType
+            )
+            networkAdapter.instance.add(NetworkServiceAdapter.postRequest(
+                "users/auth",
+                JSONObject(postParams),
+                Response.Listener<JSONObject>{response ->
+                    showLogin.text = "${response.toString()}"
+                },
+                Response.ErrorListener { response ->
+                    showLogin.text = "${response.toString()}"
+                }))
+            val intent = Intent(this, HomeTechnicalResourceActivity::class.java)
+            startActivity(intent)
+
+        }
 
     }
-
-
 }
